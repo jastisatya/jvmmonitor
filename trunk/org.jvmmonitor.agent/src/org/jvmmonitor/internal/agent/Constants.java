07edaac4a4b6a2b7d04bcce2c18008403949cdfd
yoshitaka1008@gmail.com <yoshitaka1008@gmail.com@ef11078e-a1a8-38b1-dc62-c05aa50c2aa3>
2010-10-10 22:57:00 +0000

    incremented version

--- trunk/org.jvmmonitor.agent/src/org/jvmmonitor/internal/agent/Constants.java
+++ trunk/org.jvmmonitor.agent/src/org/jvmmonitor/internal/agent/Constants.java
@@ -13,7 +13,7 @@ package org.jvmmonitor.internal.agent;
 public class Constants {
 
     /** The version. */
-    static final String VERSION = "3.6.0";
+    static final String VERSION = "3.6.1";
 
     /** The logger name. */
     static final String LOGGER_NAME = "org.jvmmonitor.agent";
