b6b609ebec834010c7c8cf6aae34c34b7900e728
yoshitaka1008 <yoshitaka1008@ef11078e-a1a8-38b1-dc62-c05aa50c2aa3>
2012-05-11 21:15:52 +0000

    preparation for release 3.8.0

--- trunk/org.jvmmonitor.agent/src/org/jvmmonitor/internal/agent/Constants.java
+++ trunk/org.jvmmonitor.agent/src/org/jvmmonitor/internal/agent/Constants.java
@@ -13,7 +13,7 @@ package org.jvmmonitor.internal.agent;
 public class Constants {
 
     /** The version. */
-    static final String VERSION = "3.7.2";
+    static final String VERSION = "3.8.0";
 
     /** The logger name. */
     static final String LOGGER_NAME = "org.jvmmonitor.agent";
